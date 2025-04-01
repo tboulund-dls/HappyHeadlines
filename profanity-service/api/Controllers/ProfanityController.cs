489b10d1bb22cb798a96bf92b0b3d92b28090182
TheDarkSide1992 <Jenscp171@gmail.com>
2025-04-01 09:52:55 +0200

    Init webapi not implemented

---
+++ profanity-service/api/Controllers/ProfanityController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProfanityController : ControllerBase
+{
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetWords()
+    {
+        return BadRequest();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Clean([FromBody] string message)
+    {
+        if (string.IsNullOrEmpty(message)) return BadRequest();
+        
+        return BadRequest();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddWord([FromBody] string? word)
+    {
+        if (string.IsNullOrEmpty(word)) return BadRequest();
+        
+        return BadRequest();
+    }
+
+    [HttpDelete]
+    [Route("{wordId}")]
+    public async Task<IActionResult> DeleteWord([FromRoute] int wordId)
+    {
+        if (wordId <= 0) return BadRequest();
+        
+        return BadRequest();
+    }
+}
---
+++ profanity-service/service/Interfaces/IService.cs
@@ -0,0 +1,10 @@
+﻿namespace service.Interfaces;
+
+public interface IService
+{
+    Task<IEnumerable<string>> getWordds();
+    Task<string> Clean(string message);
+    Task<bool> AddWord(string word);
+    Task<bool> DeleteWord(int id);
+
+}

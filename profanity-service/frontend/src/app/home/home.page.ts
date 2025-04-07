83cbdcf11121fd67dd021fd08782ad20c61f7530
Jens <55052002+TheDarkSide1992@users.noreply.github.com>
2025-04-07 16:19:01 +0200

    Profanity service/feat/frontend dokcer (#32)
    * Draft for frontend docer ionic
    
    * Added ProfanityPortal Frontend to docker
    
    * Added Ionic build to .dockerignore
    
    * re added api dependency for compose
    
    * Made small adjustments
    
    * fixed serveral issues relating to http comunication
    
    * remowed console.log stmt

--- profanity-service/frontend/src/app/home/home.page.ts
+++ profanity-service/frontend/src/app/home/home.page.ts
@@ -20,33 +20,30 @@ export class HomePage implements OnInit{
   WordFc: FormControl<string | null> = new FormControl("", [Validators.required, Validators.minLength(1), Validators.maxLength(255)]);
 
   ngOnInit() {
-    this.getMessagesMock();
+    this.getMessages();
   }
 
   async AddWord() {
     if (this.WordFc.value == null) return
     let word :WordModel = {
-      word: this.WordFc.value,
+      word: this.WordFc.value!,
     }
 
-    await firstValueFrom(this.http.post<WordModel>(environment.baseURL+"SenndMessage", word))
+    const call = this.http.post<WordModel>(environment.baseURL,word);
+    const newWord = await firstValueFrom(call);
 
-    this.getMessages()
-    console.log(word);
+    this.Words.push(newWord)
   }
 
   async DeleteWord(wordId: WordModel) {
-    await firstValueFrom(this.http.delete(environment.baseURL + 'Profanity/' + wordId.word));
+
+    await firstValueFrom(this.http.delete(environment.baseURL + '/' + wordId.word));
+
     this.getMessages()
   }
 
-  private async getMessagesMock() {
-    this.Words = [{ word: "Item 1" },{ word: "Item 2" },{ word: "Item 3" },{ word: "Item 4" },{ word: "Item 5" },{ word: "Item 6" },{ word: "Item 7" },{ word: "Item 8" },{ word: "Item 9" },{ word: "Item 10" },{ word: "Item 11" },{ word: "Item 12" },{ word: "Item 13" },{ word: "Item 14" },{ word: "Item 15" },{ word: "Item 16" },{ word: "Item 17" },{ word: "Item 18" },{ word: "Item 19" },{ word: "Item 20" }];
-    //TODO remove before production, but its fine for testing visual
-    console.log(this.Words)
-  }
   private async getMessages() {
-    const call = this.http.get<WordModel[]>(environment.baseURL + "Profanity");
+    const call = this.http.get<WordModel[]>(environment.baseURL);
     call.subscribe((resData: WordModel[]) => {
       this.Words = resData;
     })
